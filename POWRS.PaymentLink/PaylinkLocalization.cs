8c6eacae5195733ffe1edeaa5f5260762202b67a
Nenad Aleksic <nenad.aleksic@powrs.se>
2023-09-26 08:56:37 +0200

    add language files to solution

---
+++ POWRS.PaymentLink/PaylinkLocalization.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Resources;
+
+namespace POWRS.PaymentLink
+{
+    public class PaylinkLocalization
+    {
+        private readonly ResourceManager resourceManager;
+        private bool isSuported;
+        public PaylinkLocalization(string language)
+        {
+            var lang = IsSupported(language) ? language : "en-Us";
+            var cultureInfo = new CultureInfo(lang);
+            resourceManager = new ResourceManager("POWRS.PaymentLink.Resx.PaylinkResources",
+                                        typeof(PaylinkLocalization).Assembly);
+            SetCulture(cultureInfo);
+        }
+
+        private bool IsSupported(string language)
+        {
+            if (string.IsNullOrEmpty(language))
+            {
+                return false;
+            }
+
+            var supportedLanguages = new string[]
+            {
+                "en-us", "sv-se"
+            };
+
+            return supportedLanguages.Contains(language.ToLower());
+        }
+
+        private void SetCulture(CultureInfo cultureInfo)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+        }
+
+        public string GetResource(string key)
+        {
+            return resourceManager.GetString(key, CultureInfo.CurrentUICulture);
+        }
+    }
+}
