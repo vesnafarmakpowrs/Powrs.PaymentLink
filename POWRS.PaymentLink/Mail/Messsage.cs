f56acb78e02f73b41713bf28df5c6a9713762221
Nenad Aleksic <nenad.aleksic@powrs.se>
2023-09-27 15:24:03 +0200

    add method to send html body mail

---
+++ POWRS.PaymentLink/Mail/Messsage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net;
+using System.Threading.Tasks;
+using Waher.Events;
+
+namespace POWRS.PaymentLink
+{
+    public class MailSender
+    {
+        public static async Task SendHtmlMail(string host, int port, string username, string password, string recepient, string subject, string htmlContent)
+        {
+            if (string.IsNullOrEmpty(host) || string.IsNullOrEmpty(username) ||
+                string.IsNullOrEmpty(password) || string.IsNullOrEmpty(recepient) ||
+                string.IsNullOrEmpty(subject) || string.IsNullOrEmpty(htmlContent))
+            {
+                throw new Exception("Parameters missing");
+            }
+
+            try
+            {
+                var smtpClient = new SmtpClient(host)
+                {
+                    Port = port,
+                    Credentials = new NetworkCredential(username, password),
+                    EnableSsl = true,
+                };
+
+                var mailMessage = new MailMessage(username, recepient)
+                {
+                    Subject = subject,
+                    Body = htmlContent,
+                    IsBodyHtml = true,
+                };
+
+                await smtpClient.SendMailAsync(mailMessage);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
+        }
+    }
+}
