efcfa61eb94329e36f5594f647371c1731d15c42
Nenad Aleksic <nenad.aleksic@powrs.se>
2024-02-01 08:41:48 +0100

    add Settings.md to neuron admin page

---
+++ POWRS.PaymentLink/Module/PaymentLinkModule.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Waher.IoTGateway;
+
+namespace POWRS.PaymentLink.Module
+{
+    public class PaymentLinkModule : IConfigurableModule
+    {
+        public Task<IConfigurablePage[]> GetConfigurablePages()
+        {
+            return Task.FromResult(new IConfigurablePage[]
+            {
+                new ConfigurablePage("PaymentLink", "/Payout/Settings.md", "Admin.Payments.Powrs.PaymentLink")
+            });
+        }
+
+        public Task Start()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task Stop()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
