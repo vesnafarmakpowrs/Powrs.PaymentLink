ac48e4a37c72c0bc4a3a1776bf7ebfc191a570c3
Nenad Aleksic <nenad.aleksic@powrs.se>
2024-02-08 15:21:42 +0100

    .

---
+++ POWRS.PaymentLink/Models/AccountRole.cs
@@ -0,0 +1,9 @@
+﻿
+namespace POWRS.PaymentLink.Models
+{
+    public static class AccountRole
+    {
+        public static string User => nameof(User);
+        public static string Admin => nameof(Admin);
+    }
+}
---
+++ POWRS.PaymentLink/Models/BrokerAccountRole.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Waher.Persistence.Attributes;
+
+namespace POWRS.PaymentLink.Models
+{
+    [CollectionName(nameof(BrokerAccountRole) + "s")]
+    [TypeName(TypeNameSerialization.None)]
+    [Index("UserName")]
+    [Index("OrgName")]
+    public class BrokerAccountRole
+    {
+        private string objectId;
+        private string userName;
+        private string parentAccount;
+        private string role;
+        private string orgName;
+
+        public string ObjectId
+        {
+            get => objectId; 
+            set => objectId = value;
+        }
+
+        public string UserName
+        {
+            get => userName; 
+            set => userName = value;
+        }
+
+        public string ParentAccount
+        {
+            get => parentAccount; 
+            set => parentAccount = value;
+        }
+
+        public string Role
+        {
+            get => role;
+            set => role = value;
+        }
+
+        public string OrgName
+        {
+            get => orgName; 
+            set => orgName = value;
+        }
+    }
+}
