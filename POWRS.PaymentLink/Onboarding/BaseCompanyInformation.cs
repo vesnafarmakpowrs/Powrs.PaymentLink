07d71248b825dfa9f5b11c745e5132ba76ccdae2
Nenad Aleksic <nenad.aleksic@powrs.se>
2024-04-03 09:35:35 +0200

    add initial classes

---
+++ POWRS.PaymentLink/Onboarding/BaseCompanyInformation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POWRS.PaymentLink.Onboarding
+{
+    public class BaseCompanyInformation
+    {
+        private string applicantName;
+        public string ApplicantName { get => applicantName; set => applicantName = value; }
+    }
+}
---
+++ POWRS.PaymentLink/Onboarding/CompanyStructure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POWRS.PaymentLink.Onboarding
+{
+    public class CompanyStructure
+    {
+    }
+}
