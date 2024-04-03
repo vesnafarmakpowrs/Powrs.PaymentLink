4066020e3d0ecc84416fbf1800e2ba80f36efb35
VesnaFarmak <vesna.farmak@powrs.se>
2024-04-03 10:38:55 +0200

    companymodel

--- POWRS.PaymentLink/Onboarding/CompanyModel.cs
+++ POWRS.PaymentLink/Onboarding/CompanyModel.cs
@@ -3,10 +3,104 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Waher.Content.Html.Elements;
 
 namespace POWRS.PaymentLink.Onboarding
 {
     public class CompanyModel
     {
+        private string businessModel;
+        private int complaintsPerMonth;
+        private int complaintsPerYear;
+        private int daysPaymentToDelivery;
+
+        private string fullNameOwnerLargestShare;
+        private int personalNum;
+        private DateTime birthDate;
+        private string birthPlace;
+        private string addressAndPlaceOfResidence;
+
+        private int documentNumber;
+        private DateTime documentIssueDate;
+        private string documentIssueBy;
+        private string documentIssuePlace;
+
+        public string BusinessModel
+        {
+            get => businessModel;
+            set => businessModel = value;
+        }
+
+        public int ComplaintsPerMonth
+        {
+            get => complaintsPerMonth;
+            set => complaintsPerMonth = value;
+        }
+
+        public int ComplaintsPerYear
+        {
+            get => complaintsPerYear;
+            set => complaintsPerYear = value;
+        }
+
+        public int DaysPaymentToDelivery
+        {
+            get => daysPaymentToDelivery;
+            set => daysPaymentToDelivery = value;
+        }
+
+        public string FullNameOwnerLargestShare
+        {
+            get => fullNameOwnerLargestShare;
+            set => fullNameOwnerLargestShare = value;
+        }
+
+        public int PersonalNum
+        {
+            get => personalNum;
+            set => personalNum = value;
+        }
+
+        public DateTime BirthDate
+        {
+            get => birthDate;
+            set => birthDate = value;
+        }
+
+        public string BirthPlace
+        {
+            get => birthPlace;
+            set => birthPlace = value; 
+        }
+
+        public string AddressAndPlaceOfResidence
+        {
+            get => addressAndPlaceOfResidence;
+            set => addressAndPlaceOfResidence = value;
+        }
+
+        public int DocumentNumber
+        {
+            get => documentNumber;
+            set => documentNumber = value;
+        }
+
+        public DateTime DocumentIssueDate
+        {
+            get => documentIssueDate;
+            set => documentIssueDate = value;
+        }
+
+        public string DocumentIssueBy
+        {
+            get => documentIssueBy;
+            set => documentIssueBy = value;
+        }
+
+        public string DocumentIssuePlace
+        {
+            get => documentIssuePlace;
+            set => documentIssuePlace = value;
+        }
     }
 }
