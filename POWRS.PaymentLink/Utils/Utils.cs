036bdc1def39fde5252cb77b565aef68c8506e32
MirkoKruscic <mirko.kruscic@powrs.se>
2024-11-14 11:09:39 +0100

    Add Method in to Utils.cs to get all children organizations

--- POWRS.PaymentLink/Utils/Utils.cs
+++ POWRS.PaymentLink/Utils/Utils.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using POWRS.PaymentLink.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Waher.Persistence;
 
 namespace POWRS.PaymentLink
 {
@@ -38,7 +43,33 @@ namespace POWRS.PaymentLink
                 .Replace("'", "")
                 .Replace("\"", "");
         }
+
+        public static async Task<List<string>> GetAllOrganizationChildren(string parentOrgName)
+        {
+            var brokerAccounrRoleList = await Database.Find<BrokerAccountRole>();
+            int generation = 1;
+            var resultList = GetAllChildren(brokerAccounrRoleList, parentOrgName, generation).Distinct().ToList();
+
+            return resultList;
+        }
+
+        internal static List<string> GetAllChildren(IEnumerable<BrokerAccountRole> brokerAccRoleList, string parentOrgName, int generation)
+        {
+            var resultList = new List<string>();
+            var directChildren = brokerAccRoleList.Where(x => x.ParentOrgName == parentOrgName).ToList();
+
+            foreach (var item in directChildren)
+            {
+                if (!resultList.Contains(item.OrgName))
+                {
+                    resultList.Add(item.OrgName);
+                    if (generation < 100)
+                        resultList.AddRange(GetAllChildren(brokerAccRoleList, item.OrgName, generation + 1));
+                }
+            }
+
+            return resultList;
+        }
+
     }
-
-
 }
