d69953bb9f0045ec2cdebd6e4ae2437b1b6bf893
Nenad Aleksic <nenad.aleksic@powrs.se>
2024-12-19 08:52:05 +0100

    add localization for existing translations

---
+++ POWRS.PaymentLink/Localization/LocalizationService.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Resources;
+
+namespace POWRS.PaymentLink.Localization
+{
+    public class LocalizationService
+    {
+        private readonly ResourceManager _resourceManager;
+        private readonly CultureInfo _culture;
+        public LocalizationService(CultureInfo culture, string ResourceFolder)
+        {
+            // Set the default culture (e.g., Serbian Latin) or use CultureInfo.CurrentCulture for dynamic setting
+            //_culture = new CultureInfo("sr-Latn");
+            //_culture = new CultureInfo("en-US");
+            _culture = culture;
+            _resourceManager = new ResourceManager($"POWRS.PaymentLink.Localization.Resources.{ResourceFolder}.Resource", typeof(LocalizationService).Assembly);
+        }
+
+        public string Get(string key)
+        {
+            return _resourceManager.GetString(key, _culture);
+        }
+    }
+}
