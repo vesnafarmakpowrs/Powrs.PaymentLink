9a37c0274eeb8ba1ecf5dbb78129809a6e4b0e5b
Nenad Aleksic <nenad.aleksic@powrs.se>
2024-12-20 13:31:37 +0100

    add localization to html templates resource

--- POWRS.PaymentLink/Localization/Resources/HtmlTemplates/Resource.Designer.cs
+++ POWRS.PaymentLink/Localization/Resources/HtmlTemplates/Resource.Designer.cs
@@ -59,5 +59,392 @@ namespace POWRS.PaymentLink.Localization.Resources.HtmlTemplates {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Address.
+        /// </summary>
+        internal static string AddressColumn {
+            get {
+                return ResourceManager.GetString("AddressColumn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to All transactions.
+        /// </summary>
+        internal static string AllTransactionsCaption {
+            get {
+                return ResourceManager.GetString("AllTransactionsCaption", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Amount.
+        /// </summary>
+        internal static string AmountColumn {
+            get {
+                return ResourceManager.GetString("AmountColumn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Auth...
+        /// </summary>
+        internal static string AuthColumn {
+            get {
+                return ResourceManager.GetString("AuthColumn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Brand.
+        /// </summary>
+        internal static string BrandColumn {
+            get {
+                return ResourceManager.GetString("BrandColumn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Buyer Details.
+        /// </summary>
+        internal static string BuyerDetailsCaption {
+            get {
+                return ResourceManager.GetString("BuyerDetailsCaption", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to ((BuyerName )) initiated cancellation. If this is wrong please contact seller..
+        /// </summary>
+        internal static string CancellationBuyerMessage {
+            get {
+                return ResourceManager.GetString("CancellationBuyerMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to This will usually happen when payment deadline expires..
+        /// </summary>
+        internal static string CancellationDeadlineMessage {
+            get {
+                return ResourceManager.GetString("CancellationDeadlineMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to If the cancellation was not initiated by you, and you want to know more details about the contract itself, please contact the seller directly at the phone number: ((CompanyInfo.PhoneNumber )) or mail: ((CompanyInfo.Email )).
+        /// </summary>
+        internal static string CancellationNotInitiatedInfo {
+            get {
+                return ResourceManager.GetString("CancellationNotInitiatedInfo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to ((SellerName )) initiated cancellation. For more details contact seller directly..
+        /// </summary>
+        internal static string CancellationSellerMessage {
+            get {
+                return ResourceManager.GetString("CancellationSellerMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Card Details.
+        /// </summary>
+        internal static string CardDetailsCaption {
+            get {
+                return ResourceManager.GetString("CardDetailsCaption", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Please check card details and billing informations above, If something is not right, you can make changes on this ((Link )). If everything seems fine, just sit, relax, make sure that you have enough funds on the bank account and we will do the rest..
+        /// </summary>
+        internal static string CheckDetailsMessage {
+            get {
+                return ResourceManager.GetString("CheckDetailsMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to City.
+        /// </summary>
+        internal static string CityColumn {
+            get {
+                return ResourceManager.GetString("CityColumn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Code.
+        /// </summary>
+        internal static string CodeColumn {
+            get {
+                return ResourceManager.GetString("CodeColumn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Completed.
+        /// </summary>
+        internal static string CompletedStatus {
+            get {
+                return ResourceManager.GetString("CompletedStatus", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to If you want to know more details about this email and contract itself, please contact the seller directly at the phone number: ((CompanyInfo.PhoneNumber )) or mail: ((CompanyInfo.Email )).
+        /// </summary>
+        internal static string ContactSellerMessage {
+            get {
+                return ResourceManager.GetString("ContactSellerMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to This is our ((FailedPaymentAttempts )) attempt..
+        /// </summary>
+        internal static string CurrentAttempt {
+            get {
+                return ResourceManager.GetString("CurrentAttempt", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Date.
+        /// </summary>
+        internal static string DateColumn {
+            get {
+                return ResourceManager.GetString("DateColumn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Email.
+        /// </summary>
+        internal static string EmailColumn {
+            get {
+                return ResourceManager.GetString("EmailColumn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Once this this number exceeds, your product will automatically be cancelled, and in order to renew, you will have to contact seller directly..
+        /// </summary>
+        internal static string ExceedAttemptsMessage {
+            get {
+                return ResourceManager.GetString("ExceedAttemptsMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Expiration Date.
+        /// </summary>
+        internal static string ExpirationDateColumn {
+            get {
+                return ResourceManager.GetString("ExpirationDateColumn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Failed.
+        /// </summary>
+        internal static string FailedStatus {
+            get {
+                return ResourceManager.GetString("FailedStatus", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to POWRS D.O.O. BEOGRAD, Srbija.
+        /// </summary>
+        internal static string FooterCompany {
+            get {
+                return ResourceManager.GetString("FooterCompany", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to ©2021 - ((Now.Year )) POWRS.
+        /// </summary>
+        internal static string FooterCopyright {
+            get {
+                return ResourceManager.GetString("FooterCopyright", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to In accordance with the provision of Article 27 of the Consumer Protection Act, you can fill out the withdrawal form from the contract.
+        /// </summary>
+        internal static string FooterWithdrawalNote {
+            get {
+                return ResourceManager.GetString("FooterWithdrawalNote", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to We are sad to say goodbye, we hope that you had good experience from using our platform..
+        /// </summary>
+        internal static string GoodbyeMessage {
+            get {
+                return ResourceManager.GetString("GoodbyeMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Masked Pan.
+        /// </summary>
+        internal static string MaskedPanColumn {
+            get {
+                return ResourceManager.GetString("MaskedPanColumn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Note that maximum number of attempts is: ((MaxFailedAttempts ))..
+        /// </summary>
+        internal static string MaxAttemptsNote {
+            get {
+                return ResourceManager.GetString("MaxAttemptsNote", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Please note that the maximum number of retries is defined by the seller..
+        /// </summary>
+        internal static string MaxRetriesNote {
+            get {
+                return ResourceManager.GetString("MaxRetriesNote", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Name.
+        /// </summary>
+        internal static string NameColumn {
+            get {
+                return ResourceManager.GetString("NameColumn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Hello ((BuyerName )), we are sad to inform you that automatic payments for the product ((Title )) performed by the company ((SellerName )) is canceled..
+        /// </summary>
+        internal static string PaymentCanceledMessage {
+            get {
+                return ResourceManager.GetString("PaymentCanceledMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Payment canceled..
+        /// </summary>
+        internal static string PaymentCanceledTitle {
+            get {
+                return ResourceManager.GetString("PaymentCanceledTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Hello ((BuyerName )), we are happy to inform you that all due payments for ((Title )) performed by the company ((SellerName )) are now completed..
+        /// </summary>
+        internal static string PaymentCompletedMessage {
+            get {
+                return ResourceManager.GetString("PaymentCompletedMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Completed.
+        /// </summary>
+        internal static string PaymentCompletedTitle {
+            get {
+                return ResourceManager.GetString("PaymentCompletedTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Hello ((BuyerName )), we are here to inform you that payment for the product ((Title )) performed by the company ((SellerName )) just failed..
+        /// </summary>
+        internal static string PaymentFailedMessage {
+            get {
+                return ResourceManager.GetString("PaymentFailedMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Payment failed..
+        /// </summary>
+        internal static string PaymentFailedTitle {
+            get {
+                return ResourceManager.GetString("PaymentFailedTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Hello ((BuyerName )), we are here to inform you that card registration for the product ((Title )) performed by the company ((SellerName )) is not performed..
+        /// </summary>
+        internal static string PaymentNotPerformedMessage {
+            get {
+                return ResourceManager.GetString("PaymentNotPerformedMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Payment not performed..
+        /// </summary>
+        internal static string PaymentNotPerformedTitle {
+            get {
+                return ResourceManager.GetString("PaymentNotPerformedTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Phone Number.
+        /// </summary>
+        internal static string PhoneNumberColumn {
+            get {
+                return ResourceManager.GetString("PhoneNumberColumn", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Refunded.
+        /// </summary>
+        internal static string RefundedStatus {
+            get {
+                return ResourceManager.GetString("RefundedStatus", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Don&apos;t worry, we will try again few times in a next few days and will let you know how it went....
+        /// </summary>
+        internal static string RetryMessage {
+            get {
+                return ResourceManager.GetString("RetryMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Thank you for using Vaulter..
+        /// </summary>
+        internal static string ThankYouMessage {
+            get {
+                return ResourceManager.GetString("ThankYouMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to All the details about completed/failed transactions will be visible in the table below. If you have any question please contact seller directly..
+        /// </summary>
+        internal static string TransactionsInfoMessage {
+            get {
+                return ResourceManager.GetString("TransactionsInfoMessage", resourceCulture);
+            }
+        }
     }
 }
