8200281091dec89aee0c146c0290cfe009019a82
VesnaFarmak <vesna.farmak@powrs.se>
2024-12-23 12:52:41 +0100

    added isPaymentCompleted enum with states

---
+++ POWRS.PaymentLink/Contracts/Enums/Enums.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Waher.Events;
+
+namespace POWRS.PaymentLink.Contracts.Enums
+{
+    public enum PaymentCompletedStates
+    {
+        PaymentCompleted,
+        ServiceDelivered,
+        ReleasedFundsToSeller,
+        ReleaseFundsToSellerFailed,
+        AwaitingforRefundPayment,
+        RefundBuyer,
+        RefundBuyerFailed,
+        Done
+    }
+
+    public static class EnumHelper
+    {
+        public static bool IsPaymentCompleted(string State)
+        {
+            try
+            {
+                foreach (string completedState in PaymentCompletedStates.GetNames(typeof(PaymentCompletedStates)))
+                {
+                    if (State == completedState)
+                        return true;
+                }
+                return false;
+            } 
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                throw;
+            }
+}
+
+    }
+}
